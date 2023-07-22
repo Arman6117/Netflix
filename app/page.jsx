9cef5aedf1e74d99032fb88d4963835abb815d8a
Arman Patel <vanixgaming634@gmail.com>
2023-07-22 23:21:52 +0530

    Home section completed

--- app/page.jsx
+++ app/page.jsx
@@ -19,7 +19,7 @@ const Home = async () => {
     <div className="relative h-screen bg-gradient-to-b from-gray-900/10 to-[#010511] lg:h-[140vh] overflow-hidden">
       
       <Header />
-      <main>
+      <main className="relative pl-4 pb-24 lg:space-y-24 lg:px-16">
         <Banner netflixOriginals={netflixOriginals} />
       </main>
     </div>
--- components/Banner.jsx
+++ components/Banner.jsx
@@ -1,5 +1,6 @@
 "use client";
 import { baseUrl } from "@/constants/movie";
+import { InformationCircleIcon } from "@heroicons/react/solid";
 import Image from "next/image";
 import React, { useEffect, useState } from "react";
 import { FaPlay } from "react-icons/fa";
@@ -24,18 +25,19 @@ const Banner = ({ netflixOriginals }) => {
           objectFit="cover"
         />
       </div>
-      <h1 className="text-2xl lg:text-7xl md:text-4xl font-bold">
+      <h1 className="text-2xl lg:text-6xl md:text-4xl font-bold">
         {movie?.title || movie?.name || movie?.original_name}
       </h1>
-      <p className="max-w-xs text-xs md:max-w-lg md:text-lg lg:max-w-2xl lg:text-2xl">
+      <p className="max-w-xs text-shadow-md text-xs md:max-w-lg md:text-lg lg:max-w-2xl lg:text-lg">
         {movie?.overview}
       </p>
       <div className="flex space-x-3">
-        <button className="banner_button">
+        <button className="banner_button bg-white text-black">
+          
           <FaPlay className="h-4 text-black  md:h-7 md:w-7" />
-          More Info
+          Play
         </button>
-        <button className="banner_button">Play</button>
+        <button className="banner_button bg-[gray]/70">More Info <InformationCircleIcon className="h-5 w-5 md:h-8 md:w-8"/> </button>
       </div>
     </div>
   );
