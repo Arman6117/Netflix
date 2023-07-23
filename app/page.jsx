0583c57242cdc024ee9c3c3a73e0e4423dd3d970
Arman Patel <vanixgaming634@gmail.com>
2023-07-23 12:48:04 +0530

    Home section completed

--- app/page.jsx
+++ app/page.jsx
@@ -1,5 +1,6 @@
 import Banner from "@/components/Banner";
 import Header from "@/components/Header";
+import Row from "@/components/Row";
 // import { useMovies } from "@/contexts/MovieContext";
 import requests from "@/utils/requests";
 const Home = async () => {
@@ -14,13 +15,24 @@ const Home = async () => {
     documentaries,
   } = await getMoviesAndShows();
 
-  console.log("The netflix originals array: " + netflixOriginals);
+  // console.log("The netflix originals array: " + netflixOriginals);
   return (
-    <div className="relative h-screen bg-gradient-to-b from-gray-900/10 to-[#010511] lg:h-[140vh] overflow-hidden">
+    <div className="relative h-screen bg-gradient-to-b from-gray-900/10 to-[#010511] lg:h-[140vh] ">
       
       <Header />
-      <main className="relative pl-4 pb-24 lg:space-y-24 lg:px-16">
+      <main className="relative pl-4 pb-24 lg:space-y-24 lg:pl-16">
         <Banner netflixOriginals={netflixOriginals} />
+        <section className="md:space-y-24">
+        <Row title="Trending Now" movies={trendingNow} />
+          <Row title="Top Rated" movies={topRated} />
+          <Row title="Action Thrillers" movies={actionMovies} />
+          {/* My List Component */}
+          {/* {list.length > 0 && <Row title="My List" movies={list} />} */}
+          <Row title="Comedies" movies={comedyMovies} />
+          <Row title="Scary Movies" movies={horrorMovies} />
+          <Row title="Romance Movies" movies={romanceMovies} />
+          <Row title="Documentaries" movies={documentaries} />
+        </section>
       </main>
     </div>
   );
